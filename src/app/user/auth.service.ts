89201b3051567cb1025b73894630c68563f1a681
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-07 17:31:16 +0100

    collecting data with forms and validations

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { IUser } from "./user.model";
+
+@Injectable()
+
+export class AuthService {
+
+    currentUser: IUser;
+
+    loginUser(userName: string, password: string) {
+      this.currentUser = {
+          id: 1,
+          firstName: 'Fahmi',
+          lastName: 'Bouabidi',
+          userName: userName
+      }
+    }
+
+    isAuthenticated() {
+        return !!this.currentUser;
+    }
+}
